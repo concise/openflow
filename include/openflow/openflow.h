bfc1dc486c0a2f448ca3b22c1a3889cc527d49f8
Glen Gibb <grg@stanford.edu>
2009-10-29 16:13:00 -0700

    Reorder members in ofp_neighbor_msg
    Members needed reordering to ensure 64-bit alignment for 64-bit member.

--- include/openflow/openflow.h
+++ include/openflow/openflow.h
@@ -335,11 +335,10 @@ enum ofp_neighbor_activity {
 struct ofp_neighbor_msg {
     struct ofp_header header;
     uint8_t activity;               /* One of ofp_neighbor_activity */
-    uint8_t pad;                    /* Pad */
+    uint8_t pad[3];                 /* Pad */
     uint16_t local_port;            /* Local port on which neighbor is/was connected */
-    uint64_t neighbor_datapath_id;  /* Datapath id of neighbor */
     uint16_t neighbor_port;         /* Port on neighbor connected to this switch */
-    uint8_t  pad2[2];               /* Pad */
+    uint64_t neighbor_datapath_id;  /* Datapath id of neighbor */
 };
 OFP_ASSERT(sizeof(struct ofp_neighbor_msg) == 24);
 
