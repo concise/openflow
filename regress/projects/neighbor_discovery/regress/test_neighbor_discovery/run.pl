d0199ad86160616a038fa239e284f8cf7203201c
Rob Sherwood <rob.sherwood@stanford.edu>
2009-11-03 11:52:32 -0800

    Working neighbor discovery regression test
    * works for both user and kmod cases

--- regress/projects/neighbor_discovery/regress/test_neighbor_discovery/run.pl
+++ regress/projects/neighbor_discovery/regress/test_neighbor_discovery/run.pl
@@ -1,22 +1,28 @@
 #!/usr/bin/perl -w
 # test_neighbor_discovery
 #
-# Verify we get a discovery packet from the switch
-# Then send the switch a discovery packet and verify we
+# 1) Send the switch a discovery packet and verify we
 # 	get the appropriate openflow discovery msg
+#
+# 2) Wait to make sure we get a neighbor expire
+#
+# 3) Then Verify we get a discovery packet from the switch
 
 use strict;
-require OF::Includes;
+use OF::Includes;
 
-my $LLDP_format = "a6 a6 n n a8 n";
 my $LLDP_len = 26;
+my $LLDP_format = "a6 a6 n n a8 n";
+my $LLDP_destaddr = "\x01\x80\xC2\x00\x00\x0E";
+my $LLDP_srcaddr = "\x08\x00\x56\x00\x00\x00";
+
 
 sub make_lldp
 {
 	my ($dpid, $out_port,$interval) = @_;
 	my $pkt = pack $LLDP_format, (
-		"01:80:C2:00:00:0E",	# DA
-		"08:00:56:00:00:00",	# SA
+		$LLDP_destaddr,	# DA
+		$LLDP_srcaddr,	# SA
 		0x88cc,			# ethertype
 		$out_port,		# outgoing port
 		$dpid,			# datapath id
@@ -28,7 +34,7 @@ sub make_lldp
 sub parse_lldp
 {
 	my ($packet) = @_;
-	my @list = unpack $LLDP_format;
+	my @list = unpack $LLDP_format, $packet;
 	my %lldp;
 
 	die "Invalid lldp $packet :: not $LLDP_len bytes long" 
@@ -43,36 +49,104 @@ sub parse_lldp
 
 	return \%lldp;
 }
+sub dump_lldp
+{
+	my ($lldp) = @_;
+	my $key;
+	print "####### LLDP packet dump\n";
+	foreach $key (sort keys %$lldp)
+	{
+		if($key eq 'header')
+		{
+			foreach $key (sort keys %{$lldp->{'header'}})
+			{
+				print "header: $key :: " . $lldp->{'header'}->{$key} . "\n";
+			}
+		}
+		else
+		{
+			print "$key :: " . $lldp->{$key} . "\n";
+		}
+	}
+}
 
 sub my_test {
 
 	my ($sock, $options_ref) = @_;
 
 	# this could be on any port.. just making sure it happens here
-	my ($lldp, $lldp_packet, $out_lldp, $key);
+	my ($lldp, $lldp_packet, $out_lldp, $key, $recvd_mesg, @unexpected_packets);
 
 	my $port_base = $$options_ref{'port_base'};
 	my $in_port   = $port_base;
-	my $out_port  = $in_port + 1;
+	my $out_port  = $port_base;
 
-	print "Sleeping for a bit to wait for lldp to show up from switch\n";
-	sleep(10000);
-	print "Waiting for lldp from switch\n";
-	$lldp_packet = nftest_get_unexpected('eth1');
-	die "Did not get an lldp packet from switch:$!" if(not defined($lldp_packet));
+	print "port_base = $port_base\n";
+	print "in_port = $in_port\n";
+	print "out_port = $out_port\n";
+
+	############ test 1
+	# 1) build a neighbor discovery packet, 
+	# 2) send it in a port, 
+	# 3) verify we get a new neighbor message
+	$out_lldp = &make_lldp( "\x01\x02\x03\x04\x05\x06\x07\x08" , $out_port, 1);
+	nftest_send( 'eth' . $out_port, $out_lldp);
+	sysread( $sock, $recvd_mesg, 1512 ) || die "Failed to receive new neighbor message: $!";
+	
+	my $msg = $ofp->unpack( 'ofp_neighbor_msg', $recvd_mesg );
+	dump_lldp($msg);
+	die "Got command $msg->{'activity'} instead  of OFPNA_NEIGHBOR_DISCOVERED:$!" 
+		unless ($msg->{'activity'} == $enums{'OFPNA_NEIGHBOR_DISCOVERED'});
+	############## test 2
+	# 1) sleep longer than the timeout
+	# 2) verify we got the neighbor expired mesg
+	sleep(2);
+	sysread( $sock, $recvd_mesg, 1512 ) || die "Failed to receive new neighbor message: $!";
+	
+	$msg = $ofp->unpack( 'ofp_neighbor_msg', $recvd_mesg );
+	die "Got command $msg->{'activity'} instead  of OFPNA_NEIGHBOR_EXPIRED:$!" 
+		unless ($msg->{'activity'} == $enums{"OFPNA_NEIGHBOR_EXPIRED"});
+	dump_lldp($msg);
+
+	############## test 3
+	# 1) make sure we got a neighbor lldp message from the switch at some point
+	print "Waiting 12 sec for lldp from switch\n";
+	sleep(12);
+	@unexpected_packets = nftest_get_unexpected("eth" . $out_port);
+	print "Potential lldp packets = " . join(',', @unexpected_packets) . "\n";
+	die "Did not get an lldp packet from switch:$!" unless(@unexpected_packets > 0);
+	$lldp_packet = $unexpected_packets[0];	# assumes the only stray packets in this test are lldp
 	die "Got an empty lldp packet from switch:$!" if($lldp_packet eq '');
 	$lldp = &parse_lldp($lldp_packet);
-	foreach $key (sort keys %$lldp)
-	{
-		print "$key :: " . $lldp->{$key} . "\n";
-	}
-	
-	# build a neighbor discovery packet
-	$out_lldp = &make_lldp("01:02:03:04:05:06:07:08",$out_port,10);
-	
-	die("Forced Death");
 
-	# hello sequence automatically done by test harness!
+	die "Got lldp from switch with out_port $lldp->{'out_port'} instead  of 2:$!" 
+		unless ($lldp->{'out_port'} == $out_port);
+	print "LLDP has valid out_port\n";
+
+	die "Got lldp from switch with dest ether $lldp->{'DA'} instead  of lldp_multicast addr:$!" 
+		unless ($lldp->{'DA'} == $LLDP_destaddr);
+	print "LLDP has valid ether daddr\n";
+
+	die "Got lldp from switch with dest ether $lldp->{'SA'} instead  of lldp_multicast addr:$!" 
+		unless ($lldp->{'SA'} == $LLDP_srcaddr);
+	print "LLDP has valid ether saddr\n";
+
+	print "Cleaning up LLDP on other interfaces\n";
+	my $int;
+	for $int ( ('eth1', 'eth2', 'eth3', 'eth4'))
+	{
+		# ignore packets on other all other interfaces
+		@unexpected_packets = nftest_get_unexpected($int);
+		for $msg (@unexpected_packets)	# stoopid testing harness
+		{
+			if( length($msg) == $LLDP_len)	# if it's an lldp packet
+			{
+				nftest_expect($int, $msg, 1);	# ignore it; 1 == dont pad packet
+			}
+		}
+		print "Ignoring " . scalar(@unexpected_packets) . " lldp packets on interface $int\n";
+	}
+	#die("Forced Death"); for debugging
 }
 
 run_black_box_test( \&my_test, \@ARGV );
